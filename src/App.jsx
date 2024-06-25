dab4e2503ae594e0708f102a41fbfdac329c1a70
virajghatgeiv <virajghatgeiv@gmail.com>
2024-06-25 22:23:47 +0530

    created new base pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import { BrowserRouter, Route, Switch } from 'react-router-dom'
+// import Dashboard from '../src/componets/Dashboard/Dashboard'
+// import Preferences from '../src/componets/Preferences/Preferences'
+
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/componets/Preferences/Preferences.jsx
@@ -0,0 +1,7 @@
+export default function Preferences() {
+    return (
+        <>
+            <h2>Preferences</h2>
+        </>
+    );
+}
